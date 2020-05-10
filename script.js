b009b43695bd6800ad75996880e32aa2f12dfad5
Max Gadomski <46671467+m-gado01@users.noreply.github.com>
2020-05-10 09:43:35 +0200

    location

---
+++ script.js
@@ -0,0 +1,14 @@
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+
+    } else {
+        document.getElementbyId("geojson").value = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    var coordLat = position.coords.longitude;
+    var coordLong = position.coords.latitude;
+    console.log(coordLat, coordLong);
+}
