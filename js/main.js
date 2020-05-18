4b459702c82b35005fbad10446b0d3155d49d4e7
Max Gadomski <46671467+m-gado01@users.noreply.github.com>
2020-05-18 17:37:41 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -191,7 +191,7 @@ function loadWeather() {
         .attr("max", timestamp + 3600 * 24)
         .attr("step", 3600);
 
-    $("#slider").on("change", () => {
+    $("#slider").on("input", () => {
         console.log("Value changed.");
     });
 }
