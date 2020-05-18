ed76ac40d6717ff55301521ce35efa836723b659
Max Gadomski <46671467+m-gado01@users.noreply.github.com>
2020-05-18 14:28:07 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -183,7 +183,7 @@ function main(places) {
 }
 
 function loadWeather() {
-    var date = moment().locale("de").unix(Date.now());
+    var date = moment().unix(Date.now());
     $("#datetime").text(date.format("LLLL"));
 
     var timestamp = Date.now();
