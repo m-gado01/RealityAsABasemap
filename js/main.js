88893476e0565b17fbf53733c277c44c06a2207e
Max Gadomski <46671467+m-gado01@users.noreply.github.com>
2020-05-19 14:14:47 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -221,7 +221,7 @@ function loadWeather() {
             icon.setAttribute('look-at', '[gps-camera]');
             icon.setAttribute('clickhandler', true);
             icon.setAttribute('scale', '50, 50');
-            icon.setAttribute('position', '0 50 0');
+            icon.setAttribute('position', '0 -50 0');
 
             scene.appendChild(icon);
         }
