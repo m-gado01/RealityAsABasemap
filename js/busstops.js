197d5ad047ba902f4d236be3aad3d04b5217db1d
Max Gadomski <46671467+m-gado01@users.noreply.github.com>
2020-06-02 14:01:45 +0200

    Update busstops.js

--- js/busstops.js
+++ js/busstops.js
@@ -39,5 +39,36 @@ function busstopsToAR(busstops) {
 }
 
 function filterBusstops(busstops) {
-    console.log(busstops);
+    busstops.forEach((busstop) => {
+        console.log(position, busstop.geometry.coordinates);
+    });
+}
+
+function getDistance(origin, dest) {
+
+    //get coordinates of point
+    var lon1 = origin[0];
+    var lat1 = origin[1];
+    var lon2 = dest[0];
+    var lat2 = dest[1];
+
+    //degrees to radiants
+    var R = 6371e3; // metres
+    var φ1 = lat1 * (Math.PI / 180);
+    var φ2 = lat2 * (Math.PI / 180);
+    var φ3 = lon1 * (Math.PI / 180);
+    var φ4 = lon2 * (Math.PI / 180);
+    var Δφ = (lat2 - lat1) * (Math.PI / 180);
+    var Δλ = (lon2 - lon1) * (Math.PI / 180);
+
+    //calculate distances
+    var a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+        Math.cos(φ1) * Math.cos(φ2) *
+        Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    //distance
+    var d = Math.round(R * c);
+
+    return d;
 }
