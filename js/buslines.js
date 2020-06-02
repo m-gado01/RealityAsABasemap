1b3a322201c87d2fe83d2a1f31556370ec3c1df4
Max Gadomski <46671467+m-gado01@users.noreply.github.com>
2020-06-02 15:38:40 +0200

    Update buslines.js

--- js/buslines.js
+++ js/buslines.js
@@ -7,5 +7,18 @@ function initBuslines(p, s) {
 }
 
 function getBuslines(busstop) {
-    console.log(busstop);
+    var nr = busstop.properties.nr;
+    var conterra_url = "https://rest.busradar.conterra.de/prod/haltestellen" + "/" + nr + "/abfahrten?sekunden=" + 1600;
+
+    $.ajax({
+        dataType: "json",
+        url: conterra_url,
+        data: {},
+        success: function (data) {
+            console.log(data);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(textStatus, errorThrown);
+        }
+    });
 }
