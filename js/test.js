2716c62669eb75c3fd0584d1479224c7227be2d8
Max Gadomski <46671467+m-gado01@users.noreply.github.com>
2020-06-02 14:48:38 +0200

    Update test.js

--- js/test.js
+++ js/test.js
@@ -4,7 +4,7 @@ $(document).ready(() => {
     navigator.geolocation.getCurrentPosition((x) => {
         position = x;
         scene = $('a-scene')[0];
-        //initVenues(position, scene);
+        initVenues(position, scene);
         initBusstops(position, scene);
     });
 });
