bb7c6d8ab49c98335dca443f93870c387e679d85
Max Gadomski <46671467+m-gado01@users.noreply.github.com>
2020-06-09 11:44:50 +0200

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,9 +1,5 @@
 
 AFRAME.registerComponent('change-color-on-hover', {
-    schema: {
-        color: { default: 'red' }
-    },
-
     init: function () {
         var data = this.data;
         var el = this.el;  // <a-box>
