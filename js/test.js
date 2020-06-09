5031f1f7dd9e179c93884ef35ef7a4e25ce1c906
Max Gadomski <46671467+m-gado01@users.noreply.github.com>
2020-06-09 11:52:25 +0200

    Update test.js

--- js/test.js
+++ js/test.js
@@ -6,11 +6,11 @@ AFRAME.registerComponent('change-color-on-hover', {
         var el = this.el;  // <a-box>
         var defaultColor = el.getAttribute('material').color;
 
-        el.addEventListener('mouseenter', function () {
+        el.addEventListener('click', function () {
             el.setAttribute('color', data.color);
         });
 
-        el.addEventListener('mouseleave', function () {
+        el.addEventListener('click', function () {
             el.setAttribute('color', defaultColor);
         });
     }
